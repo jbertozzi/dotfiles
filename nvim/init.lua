2bf11667ab4403bb2ed2c2d2b47a5c8a5120e2be
Jeremy Bertozzi <jeremy.bertozi@gmail.com>
2022-05-04 14:34:15 +0200

    chore: git

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
--- require('settings')
-vim.cmd 'source ~/.vimrc'
-require('plugins')
-require('plugins-config.nightfox')
-require('plugins-config.telescope-fzf')
-vim.cmd 'source ~/.config/nvim/coc.vim'
-vim.cmd 'source ~/.config/nvim/telescope.vim'
