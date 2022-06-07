f4050034d782c0edd8804dcef902d97321008355
Jeremy Bertozzi <jeremy.bertozi@gmail.com>
2022-06-07 22:33:40 +0200

    feat: quickfix navigation

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -5,12 +5,13 @@ local keymap = vim.api.nvim_set_keymap
 keymap("", ",", "<Nop>", opts)
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
-
 -- next buffer
-keymap("n", "<tab>", ":bnext<cr>", opts) 
+keymap("n", "<tab>", ":bnext<cr>", opts)
 -- previous buffer
 keymap("n", "<s-tab>", ":bprevious<cr>", opts)
 -- close buffer
 keymap("n", "<leader>d", ":bd<cr>", opts)
 -- populate quickfix list from gitsigns
 -- keymap("n", "<leader>q", gs.setqflist(target=0, opts = {open = false}<cr>, opts)
+keymap("n", "<c-n>", ":cn<cr>", opts)
+keymap("n", "<c-p>", ":cp<cr>", opts)
