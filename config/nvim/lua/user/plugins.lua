d424dd9c06d47e35e7c9e9cdf86b3f20127bb47f
Jérémy Bertozzi <jeremy.bertozzi@foyer.lu>
2022-07-15 11:26:41 +0200

    feat(nvim): quickscope

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -95,6 +95,9 @@ return packer.startup(function(use)
   -- git
   use "lewis6991/gitsigns.nvim"
 
+  -- test
+  use 'unblevable/quick-scope'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
