dc8c66fc030c44fcfe94c1a75737b3d3d4b09256
Jeremy Bertozzi <jeremy.bertozi@gmail.com>
2023-01-15 14:38:13 +0100

    feat: treesiterr mapping

--- config/nvim/lua/user/treesitter.lua
+++ config/nvim/lua/user/treesitter.lua
@@ -24,4 +24,13 @@ configs.setup {
     enable = true,
     enable_autocmd = false,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      node_incremental = "<CR>",
+      scope_incremental = "<S-CR>",
+      node_decremental = "<BS>",
+    },
+  },
 }
