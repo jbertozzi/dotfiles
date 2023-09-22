f131193f64402f16e4f506f0bdf2f879605de6c5
Jérémy Bertozzi <jeremy.bertozzi@foyer.lu>
2023-09-22 07:42:09 +0200

    feat:nvim

--- config/nvim/lua/config/luasnip.lua
+++ config/nvim/lua/config/luasnip.lua
@@ -1,11 +1,37 @@
-local luasnip = require("luasnip")
+local ls = require("luasnip")
 
-vim.keymap.set({"i"}, "<leader>z", function() luasnip.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump(-1) end, {silent = true})
-
-vim.keymap.set({"i", "s"}, "<C-E>", function()
-	if luasnip.choice_active() then
-		luasnip.change_choice(1)
-	end
+vim.keymap.set({"i", "s"}, "<c-s>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  else
+  end
 end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<c-k>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, {silent = true})
+-- vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+-- vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+-- 
+-- vim.keymap.set({"i", "s"}, "<C-E>", function()
+-- 	if ls.choice_active() then
+-- 		ls.change_choice(1)
+-- 	end
+-- end, {silent = true})
+
+ls.config.set_config {
+  history = true,
+  updateevents = "TextChanged,TextChangedI",
+  enable_autosnippets = true,
+}
+
+ls.snippets = {
+  all = {
+    ls.parser.parse_snippet("expand", "# hell yeah")
+  },
+  python = {
+    ls.parser.parse_snippet("expand", "# hell yeah")
+  }
+}
