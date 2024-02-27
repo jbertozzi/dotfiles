0034175174a32bb47dc0dd4b5ddf24cff0ca7cb8
Jeremy Bertozzi <jeremy.bertozzi@foyer.lu>
2024-02-27 16:22:44 +0100

    feat: wip

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -68,7 +68,24 @@ return {
     "lewis6991/gitsigns.nvim"
   },
   {
-    'numToStr/Comment.nvim',
+    "numToStr/Comment.nvim",
     lazy = false,
   },
+  {
+    "serenevoid/kiwi.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    opts = {
+      {
+        name = "doc",
+        path = "~/doc"
+      }
+    },
+    keys = {
+      { "<leader>ww", ":lua require(\"kiwi\").open_wiki_index()<cr>", desc = "Open Wiki index" },
+      { "T", ":lua require(\"kiwi\").todo.toggle()<cr>", desc = "Toggle Markdown Task" }
+    },
+    lazy = true
+  }
 }
